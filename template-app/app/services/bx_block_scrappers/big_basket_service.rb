758a311b27b955b5e72a6a9ec40f87d85046bf89
sakshi <sakshi@arrowsoftservice.com>
2022-05-25 19:40:21 +0530

    [557723] big baskte services

--- template-app/app/services/bx_block_scrappers/big_basket_service.rb
+++ template-app/app/services/bx_block_scrappers/big_basket_service.rb
@@ -18,19 +18,24 @@ module BxBlockScrappers
           'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
       }
 
-      @base_url = "#{@append_url}product/get-products/?slug=bakery-cakes-dairy&tab_type=[%22all%22]&sorted_on=popularity&listtype=pc"
+     @base_url = "#{@append_url}product/get-products/?slug=bakery-cakes-dairy&tab_type=[%22all%22]&sorted_on=popularity&listtype=pc"
     end
 
     def scrap_data
       if is_valid_url? base_url
-        images = []
-        (2..100).each do |page|
-          resp = HTTParty.get(base_url + "&page=#{page}",headers: headers)
-          skus = resp["tab_info"]["product_map"]["all"]["prods"].each do |a|
-            images << get_detail(a['sku'])
+        file = "/home/rails/Documents/scrapping 2/scrapping/basket.csv"
+        csv_headers = [ ,"Nutritional Information", "Ingredient List"]
+        CSV.open(file, 'w', write_headers: true, headers: csv_headers) do |csv|
+          # images = []
+          (2..100).each do |page|
+            resp = HTTParty.get(base_url + "&page=#{page}",headers: headers)
+            skus = resp["tab_info"]["product_map"]["all"]["prods"].each do |a|
+              values = get_detail(a['sku'])
+              csv << [values[0]-["https://www.bbassets.com/monsters-inc/static/be1f00c92cf43c5b36397fe28c88a793.svg"],a['p_brand'], a['w'], a['pc_n'], a['mrp'], values[1].strip()]
+            end
           end
+          # images.flatten.compact rescue []
         end
-        images.flatten.compact rescue []
       else
         return false
       end
@@ -39,7 +44,8 @@ module BxBlockScrappers
     def get_detail sku
       html = HTTParty.get("#{append_url}pd/#{sku}",headers: headers)
       parsed_page = Nokogiri::HTML(html.body)
-      parsed_page.css("img._3oKVV").map{|a| a.attributes['src'].value}.compact rescue []
+      i = filter_ingradiant(parsed_page)
+      return [parsed_page.css("img._3oKVV").map{|a| a.attributes['src'].value}.compact, i] rescue []
     end
     
     private
@@ -50,5 +56,17 @@ module BxBlockScrappers
     rescue URI::InvalidURIError
       false
     end
+
+    def filter_ingradiant(parsed_page)
+      a = parsed_page.css("div._2fn-7").map{|a| a.children.text}
+      a.each{|p|  @x = p if p.include? "Nutritional Information"}
+      @x
+      # @x.split(',')
+      # xxx = xx.split(',').first.strip()
+      # xxx.slice! "Nutritional Information:"
+      # xxx
+    end
   end
 end
+
+
