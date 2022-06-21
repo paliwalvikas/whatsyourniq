9d99e3708ef87584c806ff4d90d65c62392ba2f2
Ansar <ansar@softopstechnologies.com>
2022-06-21 13:17:53 +0530

    added product file #[640859]

--- template-app/app/serializers/bx_block_catalogue/product_serializer.rb
+++ template-app/app/serializers/bx_block_catalogue/product_serializer.rb
@@ -3,8 +3,7 @@ module BxBlockCatalogue
     attributes :id, :product_name, :product_type, :product_point, :product_rating, :positive_good, :negative_not_good, :created_at, :updated_at
     
     attribute :category_type do |object|
-      category = BxBlockCategories::Category.find_by(id: object.category_id)
-      category.category_type
+      BxBlockCategories::Category.find_by(id: object.category_id)
     end
   end
 end
