cc5961d0123d71aecdf7a674419e1f6b4c893a5c
sakshi <sakshi@arrowsoftservice.com>
2022-07-08 22:38:07 +0530

    minor change

--- template-app/app/admin/product.rb
+++ template-app/app/admin/product.rb
@@ -16,7 +16,7 @@ ActiveAdmin.register BxBlockCatalogue::Product, as: 'product' do
       f.input :product_rating
       f.input :category_id, as: :select, collection: BxBlockCategories::Category.all.pluck(:category_type, :id)
       f.input :positive_good
-      f.input :images, as: :file, input_html: { multiple: true }  
+      f.input :image, as: :file, input_html: { multiple: true }  
       f.input :bar_code
       f.input :data_check
       f.input :negative_not_good
@@ -35,8 +35,8 @@ ActiveAdmin.register BxBlockCatalogue::Product, as: 'product' do
       obj.positive_good
     end
     column :negative_not_good
-    column :images do |product|
-        product.images.each do |image|
+    column :image do |product|
+        product.image.each do |image|
           div :class => "col-xs-4" do
             image_tag(image, width:80, height:80 ) if image.present?
           end
