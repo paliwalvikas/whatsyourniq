b8c5f2d95eecc031e2a68b21ad833870ff5c5070
sakshi <sakshi@arrowsoftservice.com>
2022-07-12 17:38:15 +0530

    [693638] food_basket

--- template-app/app/controllers/bx_block_catalogue/orders_controller.rb
+++ template-app/app/controllers/bx_block_catalogue/orders_controller.rb
@@ -1,19 +1,9 @@
-require 'json'
-
 module BxBlockCatalogue
   class OrdersController < ApplicationController
+    require 'json'
     include BuilderJsonWebToken::JsonWebTokenValidation
-    before_action :validate_json_web_token, :current_user, only: %i[create index]
+    before_action :validate_json_web_token, :current_user, only: %i[create index show]
     before_action :set_order, only: [:create]
-
-    def create  
-      order_item = @order.order_items.new(order_item_params) 
-      if order_item.save 
-        render json: BxBlockCatalogue::OrderSerializer.new(@order)
-      else 
-        render json: { error: order_item.errors }
-      end     
-    end
 
     def index
       order = BxBlockCatalogue::Order.where(account_id: current_user.id)
@@ -24,14 +14,26 @@ module BxBlockCatalogue
       end     
     end 
 
-    def show 
-      order = BxBlockCatalogue::Order.find_by_id(order_id: params[:order_id])
-      if order.present?
-         render json: BxBlockCatalogue::OrderSerializer.new(order)
+    def create
+      order_item = @order.order_items.new(order_item_params) 
+      if order_item.save 
+        render json: BxBlockCatalogue::OrderSerializer.new(@order)
       else 
-        render json: {error: order.errors}
-      end 
+        render json: { error: order_item.errors }
+      end     
+    end
 
+    def show 
+      order = BxBlockCatalogue::Order.find_by_id(params[:order_id])
+      if order.present?
+        data = []
+        calculation = order.order_product_calculation 
+        data << calculation
+        product =  BxBlockCatalogue::OrderSerializer.new(order)
+        render json: { nutrition_value: data, product: product } 
+      else 
+        render json: {error: "product not present"}
+      end 
     end    
 
     private
