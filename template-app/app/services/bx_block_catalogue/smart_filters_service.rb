cf56a73cd6fd75edb8335064dbc70f411b4aa4a1
sakshi <sakshi@arrowsoftservice.com>
2022-07-21 16:26:11 +0530

    [709524] issue resolved

--- template-app/app/services/bx_block_catalogue/smart_filters_service.rb
+++ template-app/app/services/bx_block_catalogue/smart_filters_service.rb
@@ -1,45 +1,57 @@
 module BxBlockCatalogue
   class SmartFiltersService
 
-  	def filters(params)
-  		data = []
-  		case params[:query]
-  		when  "food_type"
-			category = BxBlockCategories::Category.all
-	      category.each do |category|
-	        product = BxBlockCatalogue::Product.where(category_id: category.id)
-	        data << {count: product.count, food_type: category.category_type}
-	      end
-      when "category"
-        data = category(params, data)
-      # when "sub_category"
-      # 	data = sub_category(params, data)
-      end
-      data
-  	end
+  	# def filters(params)
+  	# 	data = []
+  	# 	case params[:query]
+  	# 	when  "food_type"
+			# category = BxBlockCategories::Category.all
+	  #     category.each do |category|
+	  #       product = BxBlockCatalogue::Product.where(category_id: category.id)
+	  #       data << {count: product.count, food_type: category.category_type}
+	  #     end
+   #    when "category"
+   #      data = category(params, data)
+   #    when "sub_category"
+   #    	data = sub_category(params, data)
+   #    when "niq_score"
+   #    	data = niq_score(data)
+   #    end
+   #    data
+  	# end
 
-  	private
+  	# private
 
-  	def category(params, data)
-      product = find_product(params)
-    	product.each do |prd|
-    		product =  BxBlockCatalogue::Product.where(food_drink_filter: prd)
-    		uniq = product.pluck(:category_filter).uniq
-  			filter = []
-    		uniq.map{|i| filter << {count: product.where(category_filter: i).count, category_filter: i }} 
-      	data << {count: product.count, category: prd, category_filter: filter }
-			end
-			data
-    end
+  	# def category(params, data)
+   #    product = find_product(params)
+   #  	product.each do |prd|
+   #  		product =  BxBlockCatalogue::Product.where(food_drink_filter: prd)
+   #  		uniq = product.pluck(:category_filter).uniq
+  	# 		filter = []
+   #  		uniq.map{|i| filter << {count: product.where(category_filter: i).count, category_filter: i }} 
+   #    	data << {count: product.count, category: prd, category_filter: filter }
+			# end
+			# data
+   #  end
 
-    def sub_category(params, data)
-      # product = find_product(params)
-    end
+   #  def sub_category(params, data)
+   #    product = find_product(params)
+
+   #  end
   	
-  	def find_product(params) 
-  		category_id =  BxBlockCategories::Category.category_type(params[:category_type]).ids
-      product =  BxBlockCatalogue::Product.where(category_id: category_id).pluck(:food_drink_filter).uniq
-  	end
+  	# def find_product(params)
+  	# 	category_id = BxBlockCategories::Category.category_type(params[:category_type]).ids
+   #    product =  BxBlockCatalogue::Product.where(category_id: category_id).pluck(:food_drink_filter).uniq
+  	# end
+
+  	# def niq_score(data)
+  	# 	rating = BxBlockCatalogue::Product.all.pluck(:product_rating).uniq
+  	# 	rating.each do |rat|
+  	# 	byebug
+  	# 		data << {count: BxBlockCatalogue::Product.where(product_rating: i).count, product_rating: i }
+  	# 	end
+  	# 	data
+  	# end
 
 	end
 end
