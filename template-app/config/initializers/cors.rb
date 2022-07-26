fdd2e2581a59e196fec9296d41c9c7626bef35f7
ansar <ansar@softopstechnologies.com>
2022-07-26 13:32:46 +0530

    CORS error issue fix

--- template-app/config/initializers/cors.rb
+++ template-app/config/initializers/cors.rb
@@ -1,16 +1,27 @@
-# Be sure to restart your server when you modify this file.
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  # allow do
+  #   origins '*'
+  #   resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  # end
   allow do
-    origins '*'
-
+    origins "https://ftprojectsearchengine-133842-react-native.b133842.dev.eastus.az.svc.builder.cafe"
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+  allow do
+    origins "localhost:3000"
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+  allow do
+    origins "https://ftprojectsearchengine-133842-react-native.b133842.stage.eastus.az.svc.builder.ai"
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
