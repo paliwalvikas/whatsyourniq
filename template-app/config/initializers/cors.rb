3683a530245a9b1b43b9167e72e7cfb988115898
ansar <ansar@softopstechnologies.com>
2022-07-26 17:02:32 +0530

    minor changes

--- template-app/config/initializers/cors.rb
+++ template-app/config/initializers/cors.rb
@@ -1,27 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  # allow do
-  #   origins '*'
-  #   resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  # end
   allow do
-    origins "https://ftprojectsearchengine-133842-react-native.b133842.dev.eastus.az.svc.builder.cafe"
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
-  end
-  allow do
-    origins "localhost:3000"
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
-  end
-  allow do
-    origins "https://ftprojectsearchengine-133842-react-native.b133842.stage.eastus.az.svc.builder.ai"
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+    origins '*'
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head]
   end
 end
