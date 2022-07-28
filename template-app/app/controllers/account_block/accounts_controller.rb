65574fa98c73551b80b634d0e1ee9618e2d9e0e2
ansar <ansar@softopstechnologies.com>
2022-07-28 13:14:44 +0530

    minor changes in social signup

--- template-app/app/controllers/account_block/accounts_controller.rb
+++ template-app/app/controllers/account_block/accounts_controller.rb
@@ -1,31 +1,33 @@
+# frozen_string_literal: true
+
 module AccountBlock
   class AccountsController < ApplicationController
     skip_before_action :verify_authenticity_token
-    include BuilderJsonWebToken::JsonWebTokenValidation 
+    include BuilderJsonWebToken::JsonWebTokenValidation
     before_action :validate_json_web_token, only: [:index]
 
     def create
       case params[:data][:type] #### rescue invalid API format
       when 'sms_account'
         account = SmsAccount.find_by(full_phone_number: params[:data][:attributes][:full_phone_number])
- 
+
         sms_otp = sms_otp_pin
         if account.present?
           account.register = true
           render json: SmsAccountSerializer.new(account, meta: {
-            token: encode(sms_otp.id), pin: sms_otp.pin, register: account.register
-          }).serializable_hash, status: :created
+                                                  token: encode(sms_otp.id), pin: sms_otp.pin, register: account.register
+                                                }).serializable_hash, status: :created
         else
           account = SmsAccount.new(jsonapi_deserialize(params))
           if account.save
-           render json: SmsAccountSerializer.new(account, meta: {
-            token: encode(sms_otp.id), pin: sms_otp.pin
-          }).serializable_hash, status: :created
+            render json: SmsAccountSerializer.new(account, meta: {
+                                                    token: encode(sms_otp.id), pin: sms_otp.pin
+                                                  }).serializable_hash, status: :created
           else
-            render json: {errors: format_activerecord_errors(@account.errors)},
-            status: :unprocessable_entity
+            render json: { errors: format_activerecord_errors(@account.errors) },
+                   status: :unprocessable_entity
           end
-        end   
+        end
       when 'email_account'
         account_params = jsonapi_deserialize(params)
 
@@ -34,66 +36,71 @@ module AccountBlock
 
         validator = EmailValidation.new(account_params['email'])
 
-        return render json: EmailAccountSerializer.new(account, meta: {token: encode(account.id), message: "Account already registered"}), status: :unprocessable_entity if account || !validator.valid?
+        if account || !validator.valid?
+          return render json: EmailAccountSerializer.new(account, meta: { token: encode(account.id), message: 'Account already registered' }),
+                        status: :unprocessable_entity
+        end
 
         @account = EmailAccount.new(jsonapi_deserialize(params))
         @account.platform = request.headers['platform'].downcase if request.headers.include?('platform')
         if @account.save
           # EmailAccount.create_stripe_customers(@account)
           EmailValidationMailer
-          .with(account: @account, host: request.base_url)
-          .activation_email.deliver
+            .with(account: @account, host: request.base_url)
+            .activation_email.deliver
           render json: EmailAccountSerializer.new(@account, meta: {
-            token: encode(@account.id),
-          }).serializable_hash, status: :created
+                                                    token: encode(@account.id)
+                                                  }).serializable_hash, status: :created
         else
-         render json: {errors:(@account.errors)},
-         status: :unprocessable_entity
-       end
+          render json: { errors: @account.errors },
+                 status: :unprocessable_entity
+        end
 
       when 'social_account'
-        account = SocialAccount.find_by(email: jsonapi_deserialize(params)["email"])
+        account = SocialAccount.find_by(email: jsonapi_deserialize(params)['email'])
         if account.present?
           account.register = true
           account.additional_details = true unless account.full_name.nil?
-          render json: SocialAccountSerializer.new(account, meta: {token: encode(account.id), message: "Account already registered", register: account.register, additional_details: account.additional_details }), status: :ok
+          render json: SocialAccountSerializer.new(account, meta: { token: encode(account.id), message: 'Account already registered', register: account.register, additional_details: account.additional_details }),
+                 status: :ok
         else
           account = SocialAccount.new(social_params)
           if account.save
-            render json: SocialAccountSerializer.new(account, meta: {token: encode(account.id), register: account.register}).serializable_hash, status: :created
+            render json: SocialAccountSerializer.new(account, meta: { token: encode(account.id), register: account.register }).serializable_hash,
+                   status: :created
           end
         end
       else
-       render json: {errors: [
-          {account: 'Invalid Account Type'},
-        ]}, status: :unprocessable_entity
-      end   
-    end  
-     
+        render json: { errors: [
+          { account: 'Invalid Account Type' }
+        ] }, status: :unprocessable_entity
+      end
+    end
+
     def update
       account = AccountBlock::Account.find_by_id(params[:id])
       if account.present?
         account.update(update_params)
-        account.additional_details = true 
-        render json: AccountSerializer.new(account, meta: {message: "Account updated successfully", additional_details: account.additional_details }), status: :ok
+        account.additional_details = true
+        render json: AccountSerializer.new(account, meta: { message: 'Account updated successfully', additional_details: account.additional_details }),
+               status: :ok
       else
-        render json: { message: "account not updated" }
+        render json: { message: 'account not updated' }
       end
     end
 
     def search
       @accounts = Account.where(activated: true)
-      .where('first_name ILIKE :search OR '\
+                         .where('first_name ILIKE :search OR '\
        'last_name ILIKE :search OR '\
        'email ILIKE :search', search: "%#{search_params[:query]}%")
       if @accounts.present?
-        render json: AccountSerializer.new(@accounts, meta: {message: 'List of users.'
-        }).serializable_hash, status: :ok
+        render json: AccountSerializer.new(@accounts, meta: { message: 'List of users.' }).serializable_hash,
+               status: :ok
       else
-        render json: {errors: [{message: 'Not found any user.'}]}, status: :ok
+        render json: { errors: [{ message: 'Not found any user.' }] }, status: :ok
       end
     end
-
 
     def show
       account = AccountBlock::Account.find_by_id(params[:id])
@@ -111,11 +118,13 @@ module AccountBlock
     end
 
     def update_params
-      params.require(:data).require(:attributes).permit(:full_name, :full_phone_number, :email, :activated, :image, :gender)
+      params.require(:data).require(:attributes).permit(:full_name, :full_phone_number, :email, :activated, :image,
+                                                        :gender)
     end
 
     def social_params
-      params.require(:data).require(:attributes).permit(:full_name, :full_phone_number, :email, :activated, :image, :gender, :unique_auth_id)
+      params.require(:data).require(:attributes).permit(:full_name, :full_phone_number, :email, :activated, :image_url,
+                                                        :gender, :unique_auth_id)
     end
 
     def encode(id)
@@ -124,13 +133,10 @@ module AccountBlock
 
     def sms_otp_pin
       sms_otp = SmsOtp.create(full_phone_number: params[:data][:attributes][:full_phone_number])
-    end  
+    end
 
     def search_params
       params.permit(:query)
     end
   end
-
 end
-
-
