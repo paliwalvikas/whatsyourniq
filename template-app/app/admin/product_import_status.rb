e31d1f3da9ace515fab85923ad868ee0b84c647b
ashok.rawat@taskcraft.in <ashok.rawat@taskcraft.in>
2022-09-06 19:01:13 +0530

    [761299] download product import report

--- template-app/app/admin/product_import_status.rb
+++ template-app/app/admin/product_import_status.rb
@@ -12,7 +12,22 @@ ActiveAdmin.register BxBlockCatalogue::ProductImportStatus, as: 'Product Import 
     column :error_file
 
     column :created_at
-    actions
+    actions do |report|
+      link_to 'Download report', download_admin_product_import_status_path(report, format: :pdf), class: 'view_link member_link'
+    end
   end
+
+
+  member_action :download, method: :get do
+    status = BxBlockCatalogue::ProductImportStatus.find_by(id: params[:id])
+    csv_data = CSV.parse(status.file_status)
+    csv_file = CSV.generate( encoding: 'Windows-1251' ) do |csv|
+      csv_data.each do |data|
+        csv << data
+      end
+    end
+    send_data csv_file.encode('Windows-1251'), type: 'text/csv; charset=windows-1251; header=present', disposition: "attachment; filename=accounting_report.csv"
+  end
+
 
 end
