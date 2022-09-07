2bed1c787e485490c7c2d0ebe2e9de0d9d418344
ashok.rawat@taskcraft.in <ashok.rawat@taskcraft.in>
2022-09-07 17:55:15 +0530

    [773573] removed file attachment statement

--- template-app/app/models/bx_block_catalogue/product_import_status.rb
+++ template-app/app/models/bx_block_catalogue/product_import_status.rb
@@ -1,7 +1,5 @@
 module BxBlockCatalogue
   class ProductImportStatus < BxBlockCatalogue::ApplicationRecord
     self.table_name = :product_import_statuses
-
-    has_one_attached :error_file
   end
 end
