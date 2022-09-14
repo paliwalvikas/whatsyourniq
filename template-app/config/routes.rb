dabe53caa056b0ea9cd1ce714b7adcbde240e3b8
ashok.rawat@taskcraft.in <ashok.rawat@taskcraft.in>
2022-09-14 10:13:12 +0530

    [777674] changes in data import

--- template-app/config/routes.rb
+++ template-app/config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
   get '/healthcheck', to: proc { [200, {}, ['Ok']] }
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self) rescue ActiveAdmin::DatabaseHitDuringLoad
-  require 'sidekiq/web'
-  require 'sidekiq/cron/web'
-  mount Sidekiq::Web => '/sidekiq'
+  # require 'sidekiq/web'
+  # require 'sidekiq/cron/web'
+  # mount Sidekiq::Web => '/sidekiq'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :bx_block_catalogue do
