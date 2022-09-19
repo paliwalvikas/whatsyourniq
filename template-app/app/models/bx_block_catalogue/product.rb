a25170b385dba1441e3f85ae42db7345a8ef52b0
ansar@softopstechnologies.com <ansar@softopstechnologies.com>
2022-09-19 16:04:55 +0530

    #[782295] mimor changes and calcultaion

--- template-app/app/models/bx_block_catalogue/product.rb
+++ template-app/app/models/bx_block_catalogue/product.rb
@@ -226,8 +226,8 @@ module BxBlockCatalogue
           protein_level =  'Low'
         elsif pro >= 5.4 && pro <= 10.8
           protein_level =  'Medium'
-        elsif pro > 10.8
-          protein_level =  'High'
+        elsif pro >= 10.8
+          protein_level = 'High'
         end
         data = checking_good_value(pro, 'protein', protein_level)
         value << checking_good_value(pro, 'protein', protein_level) 
@@ -365,7 +365,6 @@ module BxBlockCatalogue
       sodium = product_sodium_level
       good_ingredient << sodium[0] if sodium&.last == true 
       not_so_good_ingredient << sodium[0] if sodium&.last == false 
-      
       data = {
         good_ingredient: good_ingredient.flatten.compact,
         not_so_good_ingredient: not_so_good_ingredient.flatten
@@ -452,9 +451,9 @@ module BxBlockCatalogue
       return if sugar.zero?
       pro_sugar_val = case product_type
       when 'solid'
-        if sugar < 0.5
+        if sugar <= 0.5
           [checking_not_so_good_value(sugar, 'sugar', 'Free'), true]
-        elsif sugar >= 0.5 && sugar < 5.0
+        elsif sugar >= 0.5 && sugar <= 5.0
           [checking_not_so_good_value(sugar, 'sugar', 'Low'), true]
         elsif energy.between?(0,80) && sugar > 4.5 || energy.between?(80,160) && sugar > 9 || energy.between?(160,240) && sugar > 13.5 || energy.between?(240,320) && sugar > 18 || energy.between?(320,400) && sugar > 22.5 || energy.between?(400,480) && sugar > 27 || energy.between?(480,560) && sugar > 31 || energy.between?(560,640) && sugar > 36 || energy.between?(640, 720) && sugar > 40 || energy.between?(720, 800) && sugar > 45
           [checking_not_so_good_value(sugar, 'sugar', 'High'), false]
@@ -462,7 +461,7 @@ module BxBlockCatalogue
       when 'beverage'
         if sugar < 0.5
           [checking_not_so_good_value(sugar, 'sugar', 'Free'), true]
-        elsif sugar >= 0.5 && sugar < 2.5
+        elsif sugar >= 0.5 && sugar <= 2.5
           [checking_not_so_good_value(sugar, 'sugar', 'Free'), true]
         elsif energy.positive? && sugar.positive? || energy.between?(0,7) && sugar > 1.5 || energy.between?(7,14) && sugar > 3 || energy.between?(14,22) && sugar > 4.5 || energy.between?(22,29) && sugar > 6 || energy.between?(29,36) && sugar > 7.5 || energy.between?(36,43) && sugar > 9 || energy.between?(43,50) && sugar > 10.5 || energy.between?(50, 57) && sugar > 12 || energy.between?(57, 64) && sugar > 13.5
           [checking_not_so_good_value(sugar, 'sugar', 'High'), false]
@@ -484,7 +483,7 @@ module BxBlockCatalogue
       when 'solid'
         if sodium < 0.5
           return [checking_not_so_good_value(sodium, 'sodium', 'Free'), true]
-        elsif sodium >= 0.5 && sodium < 5.0
+        elsif sodium >= 0.5 && sodium <= 5.0
           return [checking_not_so_good_value(sodium, 'sodium', 'Low'), true]
         elsif energy.between?(0,80) && sodium > 90 || energy.between?(80,160) && sodium > 180 || energy.between?(160,240) && sodium > 270 || energy.between?(240,320) && sodium > 360 || energy.between?(320,400) && sodium > 450 || energy.between?(400,480) && sodium > 540 || energy.between?(480,560) && sodium > 630 || energy.between?(560,640) && sodium > 720 || energy.between?(640, 720) && sodium > 810 || energy.between?(720, 800) && sodium > 900
           return [checking_not_so_good_value(sodium, 'sodium', 'High'), false]
@@ -492,7 +491,7 @@ module BxBlockCatalogue
       when 'beverage'
         if sodium < 0.5
           return [checking_not_so_good_value(sodium, 'sodium', 'Free'), true]
-        elsif sodium >= 0.5 && sodium < 2.5
+        elsif sodium >= 0.5 && sodium <= 2.5
           return [checking_not_so_good_value(sodium, 'sodium', 'Low'), true]
         elsif energy.positive? && sodium > 90 || energy.between?(0,7) && sodium > 180 || energy.between?(7,14) && sodium > 270 || energy.between?(14,22) && sodium > 360 || energy.between?(22,29) && sodium > 450 || energy.between?(29,36) && sodium > 540 || energy.between?(36,43) && sodium > 630 || energy.between?(43,50) && sodium > 720 || energy.between?(50, 57) && sodium > 810 || energy.between?(57, 64) && sodium > 900
           return [checking_not_so_good_value(sodium, 'sodium', 'High'), false]
@@ -514,7 +513,7 @@ module BxBlockCatalogue
       when 'solid'
         if saturate_fat <= 0.1
           return [checking_not_so_good_value(saturate_fat, 'saturated_fat', 'Free'), true]
-        elsif saturate_fat > 0.1 && saturate_fat <= (1.5 + energy_from_saturated_fat)
+        elsif saturate_fat > 0.1 && saturate_fat <= 1.5 && energy_from_saturated_fat
           return [checking_not_so_good_value(saturate_fat, 'saturated_fat', 'Low'), true]
         elsif saturate_fat.between?(1.5, 10) && energy.between?(0,800) || saturate_fat.between?(1, 10) && energy > 800
           return [checking_not_so_good_value(saturate_fat, 'saturated_fat', 'High'), false]
@@ -522,7 +521,7 @@ module BxBlockCatalogue
       when 'beverage'
         if saturate_fat <= 0.1
           return [checking_not_so_good_value(saturate_fat, 'saturated_fat', 'Free'), true]
-        elsif saturate_fat > 0.1 && saturate_fat <= (0.75 + energy_from_saturated_fat)
+        elsif saturate_fat > 0.1 && saturate_fat <= 0.75 && energy_from_saturated_fat
           return [checking_not_so_good_value(saturate_fat, 'saturated_fat', 'Low'), true]
         elsif saturate_fat.between?(0.76, 10) && energy.between?(0,800) || saturate_fat.between?(1, 10) && energy > 800
           return [checking_not_so_good_value(saturate_fat, 'saturated_fat', 'High'), false]
@@ -535,9 +534,9 @@ module BxBlockCatalogue
       energy_from = saturate_fat * 9
       percent =  (energy_from / ingredient.energy.to_f) * 100
       value = if percent < 10
-                percent
+                 true
               else
-                0.0
+                 false
               end
     end
   end
