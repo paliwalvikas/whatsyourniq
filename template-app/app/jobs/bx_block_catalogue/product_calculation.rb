eea4d51f53b538dcb013974c8a23a96ff77c5ab8
ashok.rawat@taskcraft.in <ashok.rawat@taskcraft.in>
2022-09-20 14:26:45 +0530

    [781121] product calculation changes

--- template-app/app/jobs/bx_block_catalogue/product_calculation.rb
+++ template-app/app/jobs/bx_block_catalogue/product_calculation.rb
@@ -3,26 +3,24 @@ module BxBlockCatalogue
     queue_as :low_priority
 
     def perform
-
       product_import_status = BxBlockCatalogue::ProductImportStatus.create(job_id: "Job: #{Time.now.strftime('%Y%m%d%H%M%S')}", calculation_status: "Pending")
       csv_headers = ["Pruduct ID", "Product Name", "Calculation Status"]
-      csv_row = []
+      csv_row = []  
+      csv_row << csv_headers
 
       products = BxBlockCatalogue::Product.all
       if products.present?
         products.each do |product|
           status = product.calculation if product.bar_code.present?
           csv_row << ["#{product.id}", "#{product.product_name}", "Success"] if status
-          csv_row << ["#{product.id}", "#{product.product_name}", "Pending"] unless status
+          product_import_status.file_status = CSV.generate do |csv|
+            csv_row.each do |r_data|
+              csv << r_data
+            end
+            csv
+          end
+          product_import_status.save
         end
-      end
-
-      csv_data = csv_row.unshift(csv_headers)
-      product_import_status.file_status = CSV.generate do |csv|
-        csv_data.each do |r_data|
-          csv << r_data
-        end
-        csv
       end
 
       product_import_status.calculation_status = "Success"
