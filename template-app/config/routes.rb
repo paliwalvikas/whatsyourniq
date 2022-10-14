118a73a422377e089aea83dfe00f3cad1deeef95
sakshi <sakshi@arrowsoftservice.com>
2022-10-14 13:32:08 +0530

    [806504] np_calculation

--- template-app/config/routes.rb
+++ template-app/config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
         get :product_smart_search
         get :compare_product
         post :prod_health_preference
+        post :change_for_cal
         get :product_calculation
         delete :delete_health_preference
       end
@@ -31,6 +32,11 @@ Rails.application.routes.draw do
     resources :order_items
     resources :orders
   end
+
+  # namespace :bx_block_add_profile do
+  #   resources :add_profiles
+  #   resources :relations
+  # end
 
   namespace :account_block do
     resources :accounts
