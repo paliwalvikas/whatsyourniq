ab2d5f3afc34ee16d05cfb6353fde0c664efe90e
ashok.rawat@taskcraft.in <ashok.rawat@taskcraft.in>
2022-10-27 11:35:16 +0530

    [730913] remaining products requests spec

--- template-app/spec/requests/bx_block_catalogue/products_spec.rb
+++ template-app/spec/requests/bx_block_catalogue/products_spec.rb
@@ -235,4 +235,219 @@ RSpec.describe "Products", type: :request do
       expect(response).to have_http_status(:bad_request)
     end
   end
+
+  describe "GET /search" do
+    let(:category) { create(:category) }
+    let(:filter_category) { create(:filter_category) }
+    let(:filter_sub_category) { create(:filter_sub_category, filter_category_id: filter_category.id) }
+    let(:product) {
+      create(:product, category_id: category.id, filter_category_id: filter_category.id, filter_sub_category_id: filter_sub_category.id)
+    }
+    let(:ingredient) { create(:ingredient, product_id: product.id) }
+    let(:health_preference) { create(:health_preference, product_id: product.id) }
+    let(:user) { create(:account)}
+
+    it "should respond with Product#search" do                             
+      headers = { "ACCEPT" => "application/json" }
+      get "/bx_block_catalogue/search", :params => {
+        product: product, ingredient: ingredient, query: "THE HONEY SHOP", page: 1, per_page: 8, user: user
+      }, :headers => headers
+
+      json = JSON.parse(response.body).deep_symbolize_keys
+
+      expect(json[:products][:data]).not_to be_nil
+      
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "when Product not found" do                             
+      headers = { "ACCEPT" => "application/json" }
+      get "/bx_block_catalogue/search", :params => {
+        product: product, ingredient: ingredient, query: "THE HONEY SHOP", page: 2, per_page: 8, user: user
+      }, :headers => headers
+
+      json = JSON.parse(response.body).deep_symbolize_keys
+
+      expect(json[:errors]).to eq "Product Not Found"
+      
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "DELETE /delete_old_data" do
+    let(:category) { create(:category) }
+    let(:filter_category) { create(:filter_category) }
+    let(:filter_sub_category) { create(:filter_sub_category, filter_category_id: filter_category.id) }
+    let(:product) {
+      create(:product, category_id: category.id, filter_category_id: filter_category.id, filter_sub_category_id: filter_sub_category.id)
+    }
+    let(:ingredient) { create(:ingredient, product_id: product.id) }
+    let(:health_preference) { create(:health_preference, product_id: product.id) }
+    let(:account) { create(:account)}
+
+    it "should respond with Product#delete_old_data" do                             
+      headers = { "ACCEPT" => "application/json" }
+      delete "/bx_block_catalogue/products/delete_old_data", :params => {
+        product: product, ingredient: ingredient
+      }, :headers => headers
+
+      json = JSON.parse(response.body).deep_symbolize_keys
+
+      expect(json[:message]).to eq "deleted successfully!"
+      
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "GET /smart_search_filters" do
+    let(:category) { create(:category) }
+    let(:filter_category) { create(:filter_category) }
+    let(:filter_sub_category) { create(:filter_sub_category, filter_category_id: filter_category.id) }
+    let(:product) {
+      create(:product, category_id: category.id, filter_category_id: filter_category.id, filter_sub_category_id: filter_sub_category.id)
+    }
+    let(:ingredient) { create(:ingredient, product_id: product.id) }
+    let(:health_preference) { create(:health_preference, product_id: product.id) }
+    let(:user) { create(:account)}
+
+    it "should respond with Product#smart_search_filters" do                             
+      headers = { "ACCEPT" => "application/json" }
+      get "/bx_block_catalogue/products/smart_search_filters", :params => {
+        product: product, ingredient: ingredient, query: "category"
+      }, :headers => headers
+
+      json = JSON.parse(response.body).deep_symbolize_keys
+
+      expect(json[:category]).to eq [{:count=>0, :category=>"Packaged Cheese And Oil", :category_filter=>[]}]
+      
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "GET /product_smart_search" do
+    let(:category) { create(:category) }
+    let(:filter_category) { create(:filter_category) }
+    let(:filter_sub_category) { create(:filter_sub_category, filter_category_id: filter_category.id) }
+    let(:product) {
+      create(:product, category_id: category.id, filter_category_id: filter_category.id, filter_sub_category_id: filter_sub_category.id)
+    }
+    let(:ingredient) { create(:ingredient, product_id: product.id) }
+    let(:health_preference) { create(:health_preference, product_id: product.id) }
+    let(:account) { create(:account)}
+    let(:user) { create(:account)}
+    let(:favourite_search) { create(:favourite_search, account_id: account.id) }
+
+    it "should respond with Product#product_smart_search" do                             
+      headers = { "ACCEPT" => "application/json" }
+      get "/bx_block_catalogue/products/product_smart_search", :params => {
+        product: product, ingredient: ingredient, user: user, fav_search_id: favourite_search.id, page: 1, per: 10
+      }, :headers => headers
+
+      json = JSON.parse(response.body).deep_symbolize_keys
+
+      expect(json[:products]).not_to be_nil
+      
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "when Product not found" do                             
+      headers = { "ACCEPT" => "application/json" }
+      get "/bx_block_catalogue/products/product_smart_search", :params => {
+      }, :headers => headers
+
+      json = JSON.parse(response.body).deep_symbolize_keys
+
+      expect(json[:errors]).to eq "Product not found"
+      
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "GET /compare_product" do
+    let(:category) { create(:category) }
+    let(:filter_category) { create(:filter_category) }
+    let(:filter_sub_category) { create(:filter_sub_category, filter_category_id: filter_category.id) }
+    let(:product) {
+      create(:product, category_id: category.id, filter_category_id: filter_category.id, filter_sub_category_id: filter_sub_category.id)
+    }
+    let(:ingredient) { create(:ingredient, product_id: product.id) }
+    let(:health_preference) { create(:health_preference, product_id: product.id) }
+    let(:account) { create(:account)}
+    let(:user) { create(:account)}
+    let(:favourite_search) { create(:favourite_search, account_id: account.id) }
+
+    it "should not respond with Product#compare_product when without token" do                             
+      headers = { "ACCEPT" => "application/json" }
+      get "/bx_block_catalogue/products/compare_product", :params => {
+        product: product, ingredient: ingredient
+      }, :headers => headers
+
+      json = JSON.parse(response.body).deep_symbolize_keys
+
+      expect(json[:errors]).to eq [{:token=>"Invalid token"}]
+      
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+      expect(response).to have_http_status(:bad_request)
+    end
+  end
+
+  describe "GET /regenerate_master_data" do
+    let(:category) { create(:category) }
+    let(:filter_category) { create(:filter_category) }
+    let(:filter_sub_category) { create(:filter_sub_category, filter_category_id: filter_category.id) }
+    let(:product) {
+      create(:product, category_id: category.id, filter_category_id: filter_category.id, filter_sub_category_id: filter_sub_category.id)
+    }
+    let(:ingredient) { create(:ingredient, product_id: product.id) }
+    let(:health_preference) { create(:health_preference, product_id: product.id) }
+    let(:account) { create(:account)}
+    let(:user) { create(:account)}
+    let(:favourite_search) { create(:favourite_search, account_id: account.id) }
+
+    it "should not respond with Product#regenerate_master_data when without token" do                             
+      headers = { "ACCEPT" => "application/json" }
+      get "/bx_block_catalogue/regenerate_master_data", :params => {
+        product: product, ingredient: ingredient
+      }, :headers => headers
+
+      json = JSON.parse(response.body).deep_symbolize_keys
+
+      expect(json[:errors]).to eq [{:token=>"Invalid token"}]
+      
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+      expect(response).to have_http_status(:bad_request)
+    end
+  end
+
+  describe "GET /product_calculation" do
+    let(:category) { create(:category) }
+    let(:filter_category) { create(:filter_category) }
+    let(:filter_sub_category) { create(:filter_sub_category, filter_category_id: filter_category.id) }
+    let(:product) {
+      create(:product, category_id: category.id, filter_category_id: filter_category.id, filter_sub_category_id: filter_sub_category.id)
+    }
+    let(:ingredient) { create(:ingredient, product_id: product.id) }
+    let(:health_preference) { create(:health_preference, product_id: product.id) }
+    let(:account) { create(:account)}
+    let(:user) { create(:account)}
+    let(:favourite_search) { create(:favourite_search, account_id: account.id) }
+
+    it "should not respond with Product#product_calculation when without token" do                             
+      headers = { "ACCEPT" => "application/json" }
+      get "/bx_block_catalogue/products/product_calculation", :params => {
+        product: product, ingredient: ingredient
+      }, :headers => headers
+
+      expect(response.body).to eq "<html><body>You are being <a href=\"http://www.example.com/admin\">redirected</a>.</body></html>"
+      
+      expect(response.content_type).to eq("text/html; charset=utf-8")
+      expect(response).to have_http_status(:found)
+    end
+  end
 end
