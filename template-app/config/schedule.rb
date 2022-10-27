916ca4ff44a6e2d73f62878358f85177c98cc76b
sakshi <sakshi@arrowsoftservice.com>
2022-10-27 12:24:16 +0530

    [554458] changes

--- template-app/config/schedule.rb
+++ template-app/config/schedule.rb
@@ -1,14 +1,14 @@
-every 1.day, :at => '4:30 am' do
-  runner "BxBlockScrappers::AmamzonService.new.scrap_data"
-  runner "BxBlockScrappers::BigBasketService.new.scrap_data"
-  runner "BxBlockScrappers::BigBazaarService.new.scrap_data"
-  runner "BxBlockScrappers::FlipkartService.new.scrap_data"
-  runner "BxBlockScrappers::GreensackoService.new.scrap_data"
-  runner "BxBlockScrappers::HealthkartService.new.scrap_data"
-  runner "BxBlockScrappers::JiomartService.new.scrap_data"
-  runner "BxBlockScrappers::NatureBasketService.new.scrap_data"
-  runner "BxBlockScrappers::PaytmService.new.scrap_data"
-  runner "BxBlockScrappers::RozanaService.new.scrap_data"
-  runner "BxBlockScrappers::SnackibleService.new.scrap_data"
-  runner "BxBlockScrappers::SnacqService.new.scrap_data"
-end
+# every 1.day, :at => '4:30 am' do
+#   runner "BxBlockScrappers::AmamzonService.new.scrap_data"
+#   runner "BxBlockScrappers::BigBasketService.new.scrap_data"
+#   runner "BxBlockScrappers::BigBazaarService.new.scrap_data"
+#   runner "BxBlockScrappers::FlipkartService.new.scrap_data"
+#   runner "BxBlockScrappers::GreensackoService.new.scrap_data"
+#   runner "BxBlockScrappers::HealthkartService.new.scrap_data"
+#   runner "BxBlockScrappers::JiomartService.new.scrap_data"
+#   runner "BxBlockScrappers::NatureBasketService.new.scrap_data"
+#   runner "BxBlockScrappers::PaytmService.new.scrap_data"
+#   runner "BxBlockScrappers::RozanaService.new.scrap_data"
+#   runner "BxBlockScrappers::SnackibleService.new.scrap_data"
+#   runner "BxBlockScrappers::SnacqService.new.scrap_data"
+# end
