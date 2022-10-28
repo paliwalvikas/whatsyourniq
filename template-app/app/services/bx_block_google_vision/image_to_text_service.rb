77b1f8915d21f5abd15ebc5fd2cb959de900d042
ansar <ansar@softopstechnologies.com>
2022-10-28 10:59:16 +0530

    added jiomart service #[715246][718155][731316][740612][744109][752205][752938]

--- template-app/app/services/bx_block_google_vision/image_to_text_service.rb
+++ template-app/app/services/bx_block_google_vision/image_to_text_service.rb
@@ -1,23 +1,23 @@
-require "google/cloud/vision"
-module BxBlockGoogleVision 
-  class ImageToTextService
-    attr_accessor :image_annotator, :file_name
-    def initialize(file_name)
-      @image_annotator = Google::Cloud::Vision::ImageAnnotator.new
-      @file_name = file_name
-    end
+# require "google/cloud/vision"
+# module BxBlockGoogleVision 
+#   class ImageToTextService
+#     attr_accessor :image_annotator, :file_name
+#     def initialize(file_name)
+#       @image_annotator = Google::Cloud::Vision::ImageAnnotator.new
+#       @file_name = file_name
+#     end
 
-    def convert
-      response = image_annotator.document_text_detection image: file_name
+#     def convert
+#       response = image_annotator.document_text_detection image: file_name
       
-      text = ""
-      response.responses.each do |res|
-        res.text_annotations.each do |annotation|
-          text << annotation.description
-        end
-      end
+#       text = ""
+#       response.responses.each do |res|
+#         res.text_annotations.each do |annotation|
+#           text << annotation.description
+#         end
+#       end
 
-      text
-    end
-  end
-end
+#       text
+#     end
+#   end
+# end
