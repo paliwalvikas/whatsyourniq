1f100c010e8667551dcaa9406d750d3e80d9cd53
ashok.rawat@taskcraft.in <ashok.rawat@taskcraft.in>
2022-11-10 17:32:57 +0530

    [861758] fix: time out issue for bulk calculation bug fixes

--- template-app/app/workers/bx_block_catalogue/product_calculation_worker.rb
+++ template-app/app/workers/bx_block_catalogue/product_calculation_worker.rb
@@ -2,42 +2,41 @@ module BxBlockCatalogue
   class ProductCalculationWorker  
     include Sidekiq::Worker
     include Sidekiq::Status::Worker
-    sidekiq_options lock: :until_executed, on_conflict: { client: :log, server: :raise }
-    # sidekiq_options retry: false
+    sidekiq_options retry: false, lock: :until_executed, on_conflict: { client: :log, server: :raise }
 
     def perform(calculation_type)
       product_import_status = BxBlockCatalogue::ImportStatus.create(job_id: "Job: #{Time.now.strftime('%Y%m%d%H%M%S')}", calculation_status: "Pending")
       csv_headers = ["Pruduct ID", "Product Name", "Calculation Type", "Calculation Status"]
       csv_row = []  
       csv_row << csv_headers
-
-      products = BxBlockCatalogue::Product.all.order(:id)
     
-      products.each do |product|
-        if calculation_type == "calculate_np"
-          # if !product.np_calculated?  
-            status = product.negative_and_positive if product.bar_code.present?
-            csv_row << ["#{product.id}", "#{product.product_name}", "#{calculation_type}", "Success"] if status
-            product_import_status.file_status = CSV.generate do |csv|
-              csv_row.each do |r_data|
-                csv << r_data
-              end
-              csv
-            # end
-          end
+      BxBlockCatalogue::Product.find_in_batches(batch_size: 5000) do |products|
+        products.each do |product|
+          if calculation_type == "calculate_np"
+            # if !product.np_calculated?  
+              status = product.negative_and_positive if product.bar_code.present?
+              csv_row << ["#{product.id}", "#{product.product_name}", "#{calculation_type}", "Success"] if status
+              product_import_status.file_status = CSV.generate do |csv|
+                csv_row.each do |r_data|
+                  csv << r_data
+                end
+                csv
+              # end
+            end
 
-        elsif calculation_type == "calculate_ratings"
-          # if !product.calculated?  
-            status = product.calculation if product.bar_code.present?
-            csv_row << ["#{product.id}", "#{product.product_name}", "#{calculation_type}", "Success"] if status
-            product_import_status.file_status = CSV.generate do |csv|
-              csv_row.each do |r_data|
-                csv << r_data
-              end
-              csv
-            # end
-          end
+          elsif calculation_type == "calculate_ratings"
+            # if !product.calculated?  
+              status = product.calculation if product.bar_code.present?
+              csv_row << ["#{product.id}", "#{product.product_name}", "#{calculation_type}", "Success"] if status
+              product_import_status.file_status = CSV.generate do |csv|
+                csv_row.each do |r_data|
+                  csv << r_data
+                end
+                csv
+              # end
+            end
 
+          end
         end
         product_import_status.save
       end
