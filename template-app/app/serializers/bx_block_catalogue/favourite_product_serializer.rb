4b32fe8810e1c3927f95953654ebb0f1e95f92ee
sakshi <sakshi@arrowsoftservice.com>
2022-11-14 12:57:47 +0530

    [855787] added to compare or not

--- template-app/app/serializers/bx_block_catalogue/favourite_product_serializer.rb
+++ template-app/app/serializers/bx_block_catalogue/favourite_product_serializer.rb
@@ -2,6 +2,12 @@ module BxBlockCatalogue
   class FavouriteProductSerializer < BuilderBase::BaseSerializer
     attributes :id, :account_id, :product_id, :favourite, :created_at, :updated_at
 
+    attributes :added_to_compare do |object|
+      p_id = object&.product&.id
+      data = object&.account&.compare_products&.find_by(product_id: p_id)
+      data.present?
+    end
+
     attribute :product do |object, _params|
       BxBlockCatalogue::ProductSerializer.new(object&.product)
     end
