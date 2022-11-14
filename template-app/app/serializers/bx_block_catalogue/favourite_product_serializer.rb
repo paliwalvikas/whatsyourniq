cf7f25f86764dadac010524b787c682a1feac78c
sakshi <sakshi@arrowsoftservice.com>
2022-11-14 12:47:39 +0530

    [855787] add image to fav product

--- template-app/app/serializers/bx_block_catalogue/favourite_product_serializer.rb
+++ template-app/app/serializers/bx_block_catalogue/favourite_product_serializer.rb
@@ -2,8 +2,8 @@ module BxBlockCatalogue
   class FavouriteProductSerializer < BuilderBase::BaseSerializer
     attributes :id, :account_id, :product_id, :favourite, :created_at, :updated_at
 
-    attribute :product do |object|
-      object.product
+    attribute :product do |object, _params|
+      BxBlockCatalogue::ProductSerializer.new(object&.product)
     end
 
     attribute :account do |object|
