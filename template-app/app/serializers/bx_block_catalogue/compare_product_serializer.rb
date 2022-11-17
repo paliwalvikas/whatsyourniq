9eabd384a9dd3f3ba4c5cb58e51677e9ed18ba4b
sakshi <sakshi@arrowsoftservice.com>
2022-11-17 19:58:51 +0530

    [864416] favourite falg added

--- template-app/app/serializers/bx_block_catalogue/compare_product_serializer.rb
+++ template-app/app/serializers/bx_block_catalogue/compare_product_serializer.rb
@@ -1,5 +1,10 @@
 module BxBlockCatalogue
   class CompareProductSerializer < BuilderBase::BaseSerializer
     attributes :id, :selected, :account_id, :product_id, :created_at, :updated_at
+  
+    attribute :added_to_fav do |object|
+      compare = object&.account&.favourite_products&.where(product_id: object&.product_id)
+      compare.present? 
+    end
   end
 end
