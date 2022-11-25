9b83124d3197f11dd7513027eadff3ad0775e916
ansar <ansar@softopstechnologies.com>
2022-11-25 14:31:20 +0530

    [874718]fix: made changes in calculation

--- template-app/app/serializers/bx_block_catalogue/requested_product_serializer.rb
+++ template-app/app/serializers/bx_block_catalogue/requested_product_serializer.rb
@@ -1,28 +1,40 @@
+# frozen_string_literal: true
+
 module BxBlockCatalogue
   class RequestedProductSerializer < BuilderBase::BaseSerializer
-    attributes :name, :weight, :status, :category
+    attributes :name, :weight, :status, :category, :created_at, :updated_at
 
     attribute :product_image do |object, params|
       host = params[:host] || ''
       if object.product_image.attached?
-        object.product_image.map { |image|
+        object.product_image.map do |image|
           {
-              id: image.id,
-              url: Rails.env.development? ? (host + Rails.application.routes.url_helpers.rails_blob_url(image, only_path: true)) : image.service_url
+            id: image.id,
+            url: if Rails.env.development?
+                   (host + Rails.application.routes.url_helpers.rails_blob_url(image,
+                                                                               only_path: true))
+                 else
+                   image.service_url
+                 end
           }
-        }
+        end
       end
     end
 
     attribute :barcode_image do |object, params|
       host = params[:host] || ''
       if object.barcode_image.attached?
-        object.barcode_image.map { |image|
+        object.barcode_image.map do |image|
           {
-              id: image.id,
-              url: Rails.env.development? ? (host + Rails.application.routes.url_helpers.rails_blob_url(image, only_path: true)) : image.service_url
+            id: image.id,
+            url: if Rails.env.development?
+                   (host + Rails.application.routes.url_helpers.rails_blob_url(image,
+                                                                               only_path: true))
+                 else
+                   image.service_url
+                 end
           }
-        }
+        end
       end
     end
   end
