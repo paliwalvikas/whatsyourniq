d2f77e2fb8226779af23c9a53252a64ff223cf26
sakshi <sakshi@arrowsoftservice.com>
2022-12-02 12:02:35 +0530

    [886995] favourite serach spec

--- template-app/spec/models/bx_block_catalogue/favourite_search_spec.rb
+++ template-app/spec/models/bx_block_catalogue/favourite_search_spec.rb
@@ -5,4 +5,27 @@ RSpec.describe BxBlockCatalogue::FavouriteSearch, type: :model do
       it { should belong_to(:account).optional }
    end
 
+   subject {
+      described_class.new(
+         product_category: "",
+         product_sub_category: "",
+         niq_score: "['A']",
+         food_allergies: "['Egg']",
+         food_preference: "['Veg']",
+         functional_preference: "",
+         health_preference: "Immunity",
+         favourite: true,
+         product_count: 1,
+         added_count: 1,
+         food_type: "['Packaged Food']")
+   }
+
+   it "is valid with valid attributes" do
+      expect(subject).to be_valid
+   end
+
+   it "raises an error if Product Category not valid" do
+     subject.product_category = {:"Packaged Food"=>["Cereal based products"]}.to_json
+     expect(subject).to be_valid
+   end
 end
