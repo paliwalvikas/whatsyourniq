70f1535279a0878f7019b3ad9a60e71d30266823
sakshi@arrowsoftservice.com <sakshi@arrowsoftservice.com>
2023-01-04 12:45:20 +0000

    [910178] rspecs for chat and other api's

--- template-app/spec/factories/favourite_product_factory.rb
+++ template-app/spec/factories/favourite_product_factory.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :favourite_product, class: BxBlockCatalogue::FavouriteProduct do
     account_id { FactoryBot.create(:social_account).id }
-    product_id { FactoryBot.create(:product).id }
+    product_id { FactoryBot.create(:product, category_id: 1, filter_category_id: 1, filter_sub_category_id: 1).id }
     favourite { true }
   end
 end
