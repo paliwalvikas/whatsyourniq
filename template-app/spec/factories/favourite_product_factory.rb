319efc41aef430e2e9d5c5b103e523bd6888b5ec
sakshi@arrowsoftservice.com <sakshi@arrowsoftservice.com>
2023-01-04 06:42:36 +0000

    [910178] fav product factory

--- template-app/spec/factories/favourite_product_factory.rb
+++ template-app/spec/factories/favourite_product_factory.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :favourite_product, class: BxBlockCatalogue::FavouriteProduct do
     account_id { FactoryBot.create(:social_account).id }
-    product_id { BxBlockCatalogue::Product.last.id}
+    product_id { FactoryBot.create(:product).id }
     favourite { true }
   end
 end
