c2c5383bd06c36a7d47bbd9175637f4c0ce7d2a0
narayan.kumbhakar@qubited.com <narayan.kumbhakar@qubited.com>
2023-01-20 11:22:58 +0000

    [927119] Fix for mail not working in server

--- template-app/config/environments/production.rb
+++ template-app/config/environments/production.rb
@@ -55,6 +55,19 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              "email-smtp.ap-south-1.amazonaws.com",
+    port:                 587,
+    user_name:            "AKIA5LPHYJUXG6HIBMSF",
+    password:             "BGHgdghS/TPMIaHZE1r097itMRVUKcWLcD2cktij6Ui+",
+    authentication:       :login,
+    enable_starttls_auto: true,
+    openssl_verify_mode: "none"
+  }
+
+  config.action_mailer.perform_deliveries = true
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
