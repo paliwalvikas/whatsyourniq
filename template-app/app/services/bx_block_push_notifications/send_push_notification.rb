91436316020bab8b5db51c7816d72927a833e0a2
sakshi@arrowsoftservice.com <sakshi@arrowsoftservice.com>
2023-02-02 06:28:36 +0000

    [#933201] push notification change

--- template-app/app/services/bx_block_push_notifications/send_push_notification.rb
+++ template-app/app/services/bx_block_push_notifications/send_push_notification.rb
@@ -7,7 +7,7 @@ module BxBlockPushNotifications
       @message = message
       @account = account
       @app_url = app_url
-      @fcm_client = FCM.new(ENV['FCM_SEVER_KEY'])
+      @fcm_client = FCM.new("AAAAjmCG0wg:APA91bF48wEEPJ0p7m6ydOgzOv8B7TxKQ8CsGYicOhrTFN5mQd3vARGn9oFFMxCw8l3FFCc_9fHcO5anefy07HOlO2QsmyqzDXF_x7-sTSHEAv6kvk4eEPP-5xJWKn-sakVVApABKKmR")  #(ENV['FCM_SEVER_KEY'])
     end
 
     def call
