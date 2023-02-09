69ca7c629b783c1f4614c1a65dbb2e7b2cafa4b5
sakshi@arrowsoftservice.com <sakshi@arrowsoftservice.com>
2023-02-09 08:07:10 +0000

    [#952004] chat api change

--- template-app/app/controllers/bx_block_chat/chats_controller.rb
+++ template-app/app/controllers/bx_block_chat/chats_controller.rb
@@ -1,35 +1,35 @@
+# frozen_string_literal: true
+
 module BxBlockChat
   class ChatsController < ApplicationController
-
     def index
       data = user_chat
-      if data.present? 
+      if data.present?
         render json: data
       else
-        render json:  { message: "Chat not found" },
-                       status: :unprocessable_entity
+        render json: { message: 'Chat not found' },
+               status: :unprocessable_entity
       end
     end
 
     def list_of_chats
-      chats = chat_answers
-      render json: BxBlockChat::ChatSerializer.new(chats,  params: serializ_options).serializable_hash,
-                    status: :ok
+      chat = ac_chat_answers
+      meta = @chats.last.id == chat.last.id ? 'Completed' : 'Pending'
+      render json: { chat: BxBlockChat::ChatSerializer.new(chat, params: serializ_options), meta: meta }
     end
-
 
     def show
       chat = Chat.find_by(id: params[:id] || params[:chat_id])
       if chat.present?
         render json: ChatSerializer.new(chat).serializable_hash,
-                      status: :ok
+               status: :ok
       else
-        render json:  { message: "Chat not found" },
-                      status: :unprocessable_entity
+        render json: { message: 'Chat not found' },
+               status: :unprocessable_entity
       end
     end
 
-    private 
+    private
 
     def serializ_options
       { account: current_user, host: { host: request.protocol + request.host_with_port } }
@@ -37,26 +37,25 @@ module BxBlockChat
 
     def user_chat
       data = {}
-      ["Personal", "Lifestyle", "Health", "Nutrition"].each do |type|
+      %w[Personal Lifestyle Health Nutrition].each do |type|
         chat_ids = Chat.where(chat_type: type)&.ids
         ids = current_user&.chat_answers&.where(chat_id: chat_ids)&.pluck(:chat_id)
-        data[type] = ChatSerializer.new(Chat.where(id: ids),  params: serializ_options)
+        data[type] = ChatSerializer.new(Chat.where(id: ids), params: serializ_options)
       end
       data
     end
 
-    def chat_answers
+    def ac_chat_answers
       answer = current_user&.chat_answers&.last
-      chats = Chat.where(chat_type: params[:chat_type]).order(created_at: :asc)
-      if answer&.chat.chat_type == params[:chat_type]
-        count = chats.where(id: 0..answer&.chat_id&.to_i).count
-        chats.first(count+1)
-      elsif params[:chat_type].downcase == "personal"
-        chats.first(3)  
+      @chats = Chat.where(chat_type: params[:chat_type]).order(created_at: :asc)
+      if answer&.chat&.chat_type == params[:chat_type]
+        count = @chats.where(id: 0..answer&.chat_id&.to_i).count
+        @chats.first(count + 1)
+      elsif params[:chat_type].downcase == 'personal'
+        @chats.first(3)
       else
-        chats.first
+        @chats.first
       end
     end
-
   end
 end
--- template-app/app/serializers/bx_block_chat/chat_serializer.rb
+++ template-app/app/serializers/bx_block_chat/chat_serializer.rb
@@ -3,11 +3,25 @@ module BxBlockChat
     attributes :id, :chat_type, :question, :created_at, :updated_at
 
     attributes :answer_options do |obj|
-    	AnswerOptionSerializer.new(obj&.answer_options)
+      AnswerOptionSerializer.new(obj&.answer_options)
     end
 
     attributes :chat_answers do |obj, _params|
-    	ChatAnswerSerializer.new(obj&.chat_answers&.where(account_id: _params[:account]&.id), params: _params[:host])
+      obj = obj&.chat_answers&.find_by(account_id: _params[:account]&.id)
+      if obj.present?
+        ans =	if obj.answer.present?
+                obj.answer
+              elsif obj.answer_option_id&.present?
+                obj&.answer_option&.option
+              elsif obj.image.attached?
+                if Rails.env.development?
+                  Rails.application.routes.url_helpers.rails_blob_path(obj.image, only_path: true)
+                else
+                  obj.image&.service_url&.split('?')&.first
+                end
+              end
+      end
+      { id: obj&.id, answer: ans }
     end
   end
 end
