7dbeaa07561cf312edbdc1d4fd2fcaddb2bfc9ed
sakshi@arrowsoftservice.com <sakshi@arrowsoftservice.com>
2023-02-14 13:34:27 +0000

    [#953654] add status of chat

--- template-app/app/controllers/bx_block_chat/chats_controller.rb
+++ template-app/app/controllers/bx_block_chat/chats_controller.rb
@@ -2,6 +2,8 @@
 
 module BxBlockChat
   class ChatsController < ApplicationController
+    before_action :find_chat, only: [:list_of_chats]
+    CHAT_TYPE = %w[Personal Lifestyle Health Nutrition]
 
     def index
       data = user_chat
@@ -15,7 +17,7 @@ module BxBlockChat
 
     def list_of_chats
       chat = ac_chat_answers
-      meta = @chats&.last&.id == chat&.last&.id ? 'Completed' : 'Pending'
+      meta = meta_for_chat
       render json: { chat: BxBlockChat::ChatSerializer.new(chat, params: serialization_options), meta: meta }
     end
 
@@ -38,8 +40,8 @@ module BxBlockChat
 
     def user_chat
       data = {}
-      %w[Personal Lifestyle Health Nutrition].each do |type|
-        chat_ids = Chat.where(chat_type: type)&.ids
+      CHAT_TYPE.each do |type|
+        chat_ids = Chat.chat_type(type)&.ids
         ids = current_user&.chat_answers&.where(chat_id: chat_ids)&.pluck(:chat_id)
         data[type] = ChatSerializer.new(Chat.where(id: ids), params: serialization_options)
       end
@@ -47,8 +49,7 @@ module BxBlockChat
     end
 
     def ac_chat_answers
-      answer = current_user&.chat_answers&.last
-      @chats = Chat.where(chat_type: params[:chat_type]).order(created_at: :asc)
+      answer = find_answers(@chats&.ids).last
       if answer&.chat&.chat_type == params[:chat_type]
         count = @chats.where(id: 0..answer&.chat_id&.to_i).count
         @chats.first(count + 1)
@@ -58,5 +59,25 @@ module BxBlockChat
         @chats.first(1)
       end
     end
+
+    def find_chat
+      @chats = Chat.chat_type(params[:chat_type]).order(created_at: :asc)
+    end
+
+    def find_answers(ids)
+      current_user&.chat_answers&.where(chat_id: ids).order(created_at: :asc)
+    end
+
+    def meta_for_chat
+      meta = {}
+      CHAT_TYPE.each do |type|
+        chats = Chat.chat_type(type).order(created_at: :asc)
+        chat_answers = find_answers(chats&.ids)
+        val = ( chats.present? && chats&.last&.id == chat_answers&.last&.chat_id ) ? 'Completed' : 'Pending'
+        meta[type] = val
+      end
+      meta
+    end
+
   end
 end
